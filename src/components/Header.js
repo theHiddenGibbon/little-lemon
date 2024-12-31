58d3d4f942914c86b8851e499f0e86b79eb21cc0
theHiddenGibbon <141735220+theHiddenGibbon@users.noreply.github.com>
2024-12-31 21:49:22 +0000

    navigation bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,26 @@
 import './Header.css';
+import { useState } from 'react';
 import NavList from './NavList.js';
 import Logo from '../images/logo.svg';
 import Burger from '../images/hamburger-menu.svg';
 
 const Header = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   return(
     <header className="content-grid">
       <nav className="head-nav">
         <div className="logo-container">
-          <button className="burger">
+          <button className="burger" onClick={toggleMenu}>
             <img src={Burger} alt="Menu icon" className="burger-icon" />
           </button>
           <img src={Logo} alt="Little Lemon logo" className="logo" />
         </div>
-        <NavList class="nav-bar" />
+        <NavList class={`nav-bar ${menuVisible ? 'visible' : ''}`} />
       </nav>
     </header>
   );
